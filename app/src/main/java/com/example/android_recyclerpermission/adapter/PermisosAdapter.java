9282280f01cfe4ef715a74d71379cc0f0a328863
Jess JH <lizethjh17@gmail.com>
2023-11-05 20:21:05 -0600

    Update

--- app/src/main/java/com/example/android_recyclerpermission/adapter/PermisosAdapter.java
+++ app/src/main/java/com/example/android_recyclerpermission/adapter/PermisosAdapter.java
@@ -1,12 +1,22 @@
 package com.example.android_recyclerpermission.adapter;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CompoundButton;
 import android.widget.Switch;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.android_recyclerpermission.R;
@@ -27,7 +37,7 @@ public class PermisosAdapter extends RecyclerView.Adapter<PermisosAdapter.ViewHo
     public PermisosAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
         View ly = layoutInflater.inflate(R.layout.permisoitem, parent, false);
-        return new ViewHolder(ly);
+        return new ViewHolder(ly, (Activity) parent.getContext());
     }
 
     @Override
@@ -38,17 +48,48 @@ public class PermisosAdapter extends RecyclerView.Adapter<PermisosAdapter.ViewHo
 
     @Override
     public int getItemCount() {
-       return LP.size();
+        return LP.size();
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder {
         Permiso PermisoH;
         TextView txtPermiso;
         Switch swPermiso;
-        public ViewHolder(@NonNull View itemView) {
+        Activity activity;
+
+        public ViewHolder(@NonNull View itemView, Activity activity) {
             super(itemView);
             txtPermiso = itemView.findViewById(R.id.tvPermiso);
             swPermiso = itemView.findViewById(R.id.switch1);
+            this.activity = activity;
+
+            swPermiso.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+                @Override
+                public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                    if (isChecked) {
+                        Context context = itemView.getContext();
+                        if (ContextCompat.checkSelfPermission(context, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+                            ActivityCompat.requestPermissions((Activity) context, new String[]{Manifest.permission.CALL_PHONE}, 3321);
+                        }
+
+                        if (ContextCompat.checkSelfPermission(context, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+                            ActivityCompat.requestPermissions((Activity) context, new String[]{Manifest.permission.CAMERA}, 1121);
+                        }
+                    } else {
+
+                    }
+                }
+            });
+        }
+
+        public void Camara() {
+            Intent intentCamara = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            activity.startActivity(intentCamara);
+        }
+
+        public void llamar(){
+            Intent intentcall = new Intent(Intent.ACTION_CALL, Uri.parse("tel: 8714298679"));
+            activity.startActivity(intentcall);
         }
 
         public void setData(Permiso permiso) {
